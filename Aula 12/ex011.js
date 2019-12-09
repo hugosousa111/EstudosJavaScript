6cf9d70d6c2b28fbf67ae7aa74a95b4a0c60900e
hugosousa111 <hugosousa1111@alu.ufc.br>
2019-12-09 11:55:29 -0300

    Aula de condições e switch

---
+++ Aula 12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 16
+if (idade<16){
+    console.log('Não Vota')
+}else{
+    if(idade>=16 && idade<18){
+        console.log('Voto Opcional')
+    }else{
+        console.log('VOTA')
+    }
+    
+}
