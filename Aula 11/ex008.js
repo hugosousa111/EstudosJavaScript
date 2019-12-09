0e7afe107d976a68b58c6705f64dad798a5b0b64
hugosousa111 <hugosousa1111@alu.ufc.br>
2019-12-09 11:22:27 -0300

    Aula 10 condições

---
+++ Aula 11/ex008.js
@@ -0,0 +1,10 @@
+console.log("FUNCIONOU")
+var vel = 60.5
+if (vel>60){
+    console.log(`Voce ultrapassou o limite de 60 km/h`)
+    console.log(`A sua velocidade é ${vel} por hora`)
+}
+
+console.log("Dirija com cuidado")
+
+//comment
---
+++ Aula 11/ex009.js
@@ -0,0 +1,8 @@
+var pais = "eua"
+if (pais == "bra"){
+    console.log(`brasileiro`)
+}else{
+    console.log("ñ brasileiro")
+}
+
+//comment
